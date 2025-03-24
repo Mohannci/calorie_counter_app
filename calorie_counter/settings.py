cf1719e4fd060318eb21b8b9d6f5dc65ca5075c0
Mohan <x23418613@student.ncirl.ie>
2025-03-24 17:51:26 +0530

    static files

--- calorie_counter/settings.py
+++ calorie_counter/settings.py
@@ -108,9 +108,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = "/staticfiles/" """
 
 STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = "staticfiles/"
-
