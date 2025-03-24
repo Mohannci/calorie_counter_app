d4b008949b6047a3e9a538773b38993ab2fcd193
Mohan <x23418613@student.ncirl.ie>
2025-03-24 17:45:07 +0530

    Update settings.py

--- calorie_counter/settings.py
+++ calorie_counter/settings.py
@@ -103,6 +103,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+""" STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = "/staticfiles/"
+STATIC_ROOT = "/staticfiles/" """
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  
+]
+STATIC_ROOT = "staticfiles/"
+
